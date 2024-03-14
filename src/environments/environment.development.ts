9fbbeb52d6cac16628cf4c88302055a399fe6fdc
kevin <salvikevin2@gmail.com>
2024-03-14 18:38:31 +0530

    changes

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,4 +1,5 @@
 export const environment = {
     production:false,
-    apiurl:'https://localhost:44377/api/'
+    apiurl:'https://localhost:44377/api/',
+    huburl:'https://localhost:44377/hubs/',
 };
